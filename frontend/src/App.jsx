5a326b8ed1501aa45fc29ec9e7a4ebba04351678
Thomas Jay Rush <jrush@quickblocks.io>
2023-12-25 07:46:32 -0500

    First attempt logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
+import logo from './assets/images/logo.png';
 import './App.css';
 import {Export} from "../wailsjs/go/main/App";
 
