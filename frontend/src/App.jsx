983102df090ec02acb2d3253e6a30885a77b5720
Thomas Jay Rush <jrush@quickblocks.io>
2023-12-25 19:27:02 -0500

    frontend code and spacing improvements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,53 @@ import './App.css';
 import {Export} from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
     const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
+    const [resultText, setResultText] = useState(undefined);
+    const [loading, setLoading] = useState(false);
 
-    function exportTxs() {
-        Export(name).then(updateResultText);
+    const exportTxs = async () => {
+        setLoading(true);
+        setResultText(undefined);
+        const result = await Export(name);
+        setResultText(result);
+        setLoading(false);
     }
 
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result"><pre>{resultText}</pre></div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} placeholder="trueblocks.eth" autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={exportTxs}>Export</button>
+            <img src={logo} alt="logo"/>
+            <div className="address-prompt">
+                Enter an address or ENS name below 👇
             </div>
+            <div className="input-box">
+                <input 
+                    className="input" 
+                    onChange={(e) => setName(e.target.value)} 
+                    onKeyDown={(e) => e.key === 'Enter' && exportTxs()}
+                    value={name}
+                    placeholder="trueblocks.eth" 
+                    autoComplete="off" 
+                    name="input" 
+                    autoFocus
+                />
+                <button 
+                    className="btn" 
+                    onClick={exportTxs}
+                    disabled={loading || name === ''}
+                >
+                    Export
+                </button>
+            </div>
+            {loading &&
+                <div className="result">
+                    Loading...
+                </div>
+            }
+            {resultText !== undefined &&
+                <div className='result'>
+                    {resultText}
+                </div>
+            }
         </div>
     )
 }
